811c9699ac1cd2e453e38f5a13543d61cd42a573
aydnorcn <orcnaydn.35@hotmail.com>
2026-08-18 14:39:57 +0300

    feat: implement soft delete and user foreign key to url model

--- models/url.go
+++ models/url.go
@@ -4,10 +4,12 @@ import "time"
 
 type URL struct {
 	ID          uint   `gorm:"primaryKey"`
+	OwnerUser   User   `gorm:"not null; foreignKey:OwnerUserId"`
 	OriginalURL string `gorm:"not null"`
 	ShortCode   string `gorm:"uniqueIndex;not null"`
 	ExpiresAt   *time.Time
 	IsActive    bool `gorm:"default:true"`
+	IsDeleted   bool `gorm:"default:false;index"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
